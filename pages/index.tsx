0ee19c1747a77424fa5f4da46506af39afcac097
mvargas33 <mvargas33@protonmail.com>
2023-12-10 23:15:05 -0300

    Add image and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,8 @@ import Particles from "react-tsparticles";
 import type { Container, Engine } from "tsparticles-engine";
 import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 import { useCallback } from 'react';
-import { useGetContractState } from '../integrations/source-cccb/hooks';
+import chartImage from './assets/chart.png';
+import Image from 'next/image'
 
 const SourceContractState = dynamic(() => import('./components/source-contract-state'), { ssr: false })
  
@@ -55,6 +56,19 @@ const MainTitleContainer = styled('div')({
   zIndex: 1,
 });
 
+const ImageContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  color: 'white',
+  fontSize: '4rem',
+  fontWeight: 800,
+  margin: '2rem 0 0 0',
+  justifyContent: 'center',
+  width: '100%',
+  zIndex: 1,
+});
+
 const MainTitle = styled('div')({
   fontSize: '4rem',
   fontWeight: 800,
@@ -63,6 +77,18 @@ const MainTitle = styled('div')({
 
 const Subtitle = styled('div')({
   fontSize: '2rem',
+  fontWeight: 200,
+  width: '100%',
+});
+
+const StyledUl = styled('li')({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center'
+});
+
+const StyledLy = styled('p')({
+  fontSize: '1.4rem',
   fontWeight: 200,
   width: '100%',
 });
@@ -93,8 +119,19 @@ const particlesLoaded = useCallback(async (container: Container | undefined) => 
   return (
     <MainContainer>
         <MainTitleContainer>
-          <MainTitle>Save up to 50% in bridges</MainTitle>
+          <MainTitle>Save up to 80% in bridges</MainTitle>
           <Subtitle>Thanks to Chainlink CCIP</Subtitle>
+          <Subtitle style={{ marginTop: '7rem'}}>Bridge with other participants in one CCIP transaction, to save gas. A normal CCIP bridge cost around 235,000 gas, but using this protocol, you can pay as lowe as 45,000 gas</Subtitle>
+          <ImageContainer>
+            <Image src={chartImage} alt="Chart"/>
+          </ImageContainer>
+          <Subtitle>The more the participants, the lower the gas fee. </Subtitle>
+          <ul>
+          <StyledLy>With 14 participants, you pay 65,000 gas (shown as triangle in the chart). The cost of bridge equals the cost of an ERC20 transfer!</StyledLy>
+          <StyledLy>With 19 participants, you pay 60,000 gas (shown as square in the chart)</StyledLy>
+          <StyledLy>With 60 participants, you pay 50,000 gas (shown as circle in the chart)</StyledLy>
+          <StyledLy>With 778 (max) participants, you pay: 45,000 gas. Which is max allowed (CCIP limitations)</StyledLy>
+        </ul>
         </MainTitleContainer>
         <Divider/>
         <ConnectButtonContainer>
